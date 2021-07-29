d8593a70b8580f45434211819ad56b784a00251d
Lars Sidén <lars.siden@softronic.se>
2021-07-29 21:31:21 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace WinWifi
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.tb = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.button3);
+			this.splitContainer1.Panel1.Controls.Add(this.button2);
+			this.splitContainer1.Panel1.Controls.Add(this.button1);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.tb);
+			this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+			this.splitContainer1.SplitterDistance = 165;
+			this.splitContainer1.TabIndex = 0;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(38, 166);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(38, 98);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(38, 28);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// tb
+			// 
+			this.tb.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.tb.Location = new System.Drawing.Point(0, 0);
+			this.tb.Multiline = true;
+			this.tb.Name = "tb";
+			this.tb.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.tb.Size = new System.Drawing.Size(631, 450);
+			this.tb.TabIndex = 0;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.splitContainer1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			this.splitContainer1.Panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox tb;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using NativeWifi;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinWifi
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+      WlanClient client = new WlanClient();
+      foreach (WlanClient.WlanInterface wlanIface in client.Interfaces)
+      {
+        // Lists all networks with WEP security
+        Wlan.WlanAvailableNetwork[] networks = wlanIface.GetAvailableNetworkList(0);
+        foreach (Wlan.WlanAvailableNetwork network in networks)
+        {
+          tb.AppendText("Found network with SSID: " + GetStringForSSID(network.dot11Ssid) +"\r\n");
+        }
+
+        // Retrieves XML configurations of existing profiles.
+        // This can assist you in constructing your own XML configuration
+        // (that is, it will give you an example to follow).
+        foreach (Wlan.WlanProfileInfo profileInfo in wlanIface.GetProfiles())
+        {
+          string name = profileInfo.profileName; // this is typically the network's SSID
+          string xml = wlanIface.GetProfileXml(profileInfo.profileName);
+
+          tb.AppendText(xml + "\r\n");
+        }
+
+        // Connects to a known network with WEP security
+        //string profileName = "Cheesecake"; // this is also the SSID
+        //string mac = "52544131303235572D454137443638";
+        //string key = "hello";
+        //string profileXml = string.Format("<?xml version=\"1.0\"?><WLANProfile xmlns=\"http://www.microsoft.com/networking/WLAN/profile/v1\"><name>{0}</name><SSIDConfig><SSID><hex>{1}</hex><name>{0}</name></SSID></SSIDConfig><connectionType>ESS</connectionType><MSM><security><authEncryption><authentication>open</authentication><encryption>WEP</encryption><useOneX>false</useOneX></authEncryption><sharedKey><keyType>networkKey</keyType><protected>false</protected><keyMaterial>{2}</keyMaterial></sharedKey><keyIndex>0</keyIndex></security></MSM></WLANProfile>", profileName, mac, key);
+        //wlanIface.SetProfile(Wlan.WlanProfileFlags.AllUser, profileXml, true);
+        //wlanIface.Connect(Wlan.WlanConnectionMode.Profile, Wlan.Dot11BssType.Any, profileName);
+      }
+    }
+
+    static string GetStringForSSID(Wlan.Dot11Ssid ssid)
+    {
+      return Encoding.ASCII.GetString(ssid.SSID, 0, (int)ssid.SSIDLength);
+    }
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+      var wlanClient = new WlanClient();
+      foreach (WlanClient.WlanInterface wlanInterface in wlanClient.Interfaces)
+      {
+        Wlan.WlanBssEntry[] wlanBssEntries = wlanInterface.GetNetworkBssList();
+        foreach (Wlan.WlanBssEntry wlanBssEntry in wlanBssEntries)
+        {
+          byte[] macAddr = wlanBssEntry.dot11Bssid;
+          var macAddrLen = (uint)macAddr.Length;
+          var str = new string[(int)macAddrLen];
+          for (int i = 0; i < macAddrLen; i++)
+          {
+            str[i] = macAddr[i].ToString("x2");
+          }
+          string mac = string.Join("", str);
+          tb.AppendText (GetStringForSSID(wlanBssEntry.dot11Ssid) + " : " + mac +  "\r\n");
+        }
+      }
+    }
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+      var wlanClient = new WlanClient();
+      
+      //foreach (WlanClient.WlanInterface wlanInterface in wlanClient.Interfaces)
+      {
+        var MacAdressOfConnectedAP = wlanClient.Interfaces[0].CurrentConnection.wlanAssociationAttributes.Dot11Bssid;
+        var ssid = Encoding.UTF8.GetString(wlanClient.Interfaces[0].CurrentConnection.wlanAssociationAttributes.dot11Ssid.SSID).Replace("\0", string.Empty); ;
+        uint tx = wlanClient.Interfaces[0].CurrentConnection.wlanAssociationAttributes.txRate;
+        uint rx = wlanClient.Interfaces[0].CurrentConnection.wlanAssociationAttributes.rxRate;
+
+        string MAC = MacAdressOfConnectedAP.ToString();
+        var output = string.Join(":", Enumerable.Range(0, 6)
+          .Select(i => MAC.Substring(i * 2, 2)));
+
+        var s = $"{ssid} : {MAC} : {rx / 1000} RX mbps {tx / 1000} TX mbps\r\n";
+
+        tb.AppendText(s);
+      }
+    }
+	}
+}
